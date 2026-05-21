1fb38c715da4f4dbc957c71f45f083814f5dbd59
Srishtiverma12 <srishtiverma0912@gmail.com>
2026-05-21 09:01:50 +0530

    Add express server for Railway frontend

---
+++ client/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 4173;
+app.listen(PORT, () => {
+  console.log(`Frontend server running on port ${PORT}`);
+});
